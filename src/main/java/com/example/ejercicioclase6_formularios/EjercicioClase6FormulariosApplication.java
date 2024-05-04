9451b2f4bd747aa38e0b7b55eee240e96544684e
Yosthim <a20201497@pucp.edu.pe>
2024-05-03 19:06:08 -0500

    Proyecto base

---
+++ src/main/java/com/example/ejercicioclase6_formularios/EjercicioClase6FormulariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.ejercicioclase6_formularios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioClase6FormulariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EjercicioClase6FormulariosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ejercicioclase6_formularios/EjercicioClase6FormulariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ejercicioclase6_formularios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioClase6FormulariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
